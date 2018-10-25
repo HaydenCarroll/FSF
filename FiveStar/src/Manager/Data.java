c30e89562c8cf2990f03f643c35c0fef565d1bb5
disasterpiece9000 <35543856+disasterpiece9000@users.noreply.github.com>
2018-10-25 13:55:13 -0400

    Added math methods for Ads and Jobs

--- FiveStar/src/Manager/Data.java
+++ FiveStar/src/Manager/Data.java
@@ -139,5 +139,101 @@ public class Data {
 		}
 		return materialList.size()-1;
 	}
+	
+	public static Ad getMostSeenAd(ArrayList<Ad> ads) {
+		int maxUses = ads.get(0).getNumberOfUses();
+		Ad bestAd = ads.get(0);
+		for(int i = 1; i < ads.size(); i++) {
+			if( ads.get(i).getNumberOfUses() > maxUses) {
+				maxUses = ads.get(i).getNumberOfUses();
+				bestAd = ads.get(i);
+			}
+		}
+		return bestAd;
+	}
+	
+	public static Ad getMostValueAd(ArrayList<Ad> ads) {
+		Ad bestAd = ads.get(0);
+		double value = ads.get(0).getNumberOfUses() / ads.get(0).getAdCost();
+		for(int i = 1; i < ads.size(); i++) {
+			if (ads.get(i).getNumberOfUses() / ads.get(i).getAdCost() > value) {
+				value = ads.get(i).getNumberOfUses() / ads.get(i).getAdCost();
+				bestAd = ads.get(i);
+			}
+		}
+		return bestAd;
+	}
+	
+	public static Ad getMostSoldAd(ArrayList<Ad> ads) {
+		Ad bestAd = ads.get(0);
+		double value = ads.get(0).getAmtSold() / ads.get(0).getAdCost();
+		for (int i = 1; i < ads.size(); i++) {
+			if (ads.get(i).getAmtSold() / ads.get(i).getAdCost() > value) {
+				value = ads.get(i).getAmtSold() / ads.get(i).getAdCost();
+				bestAd = ads.get(i);
+			}
+		}
+		return bestAd;
+	}
+		
+	public static Ad getLeastSeenAd(ArrayList<Ad> ads) {
+		int minUses = ads.get(0).getNumberOfUses();
+		Ad worstAd = ads.get(0);
+		for(int i = 1; i < ads.size(); i++) {
+			if( ads.get(i).getNumberOfUses() < minUses) {
+				minUses = ads.get(i).getNumberOfUses();
+				worstAd = ads.get(i);
+			}
+		}
+		return worstAd;
+	}
+	
+	public static Ad getLeastValueAd(ArrayList<Ad> ads) {
+		Ad worstAd = ads.get(0);
+		double value = ads.get(0).getNumberOfUses() / ads.get(0).getAdCost();
+		for(int i = 1; i < ads.size(); i++) {
+			if (ads.get(i).getNumberOfUses() / ads.get(i).getAdCost() > value) {
+				value = ads.get(i).getNumberOfUses() / ads.get(i).getAdCost();
+				worstAd = ads.get(i);
+			}
+		}
+		return worstAd;
+	}
+	
+	public static Ad getLeastSoldAd(ArrayList<Ad> ads) {
+		Ad worstAd = ads.get(0);
+		double value = ads.get(0).getAmtSold() / ads.get(0).getAdCost();
+		for (int i = 1; i < ads.size(); i++) {
+			if (ads.get(i).getAmtSold() / ads.get(i).getAdCost() > value) {
+				value = ads.get(i).getAmtSold() / ads.get(i).getAdCost();
+				worstAd = ads.get(i);
+			}
+		}
+		return worstAd;
+	}
+	
+	public static Job getMostlaborJob(ArrayList<Job> jobs) {
+		Job mostLabor = jobs.get(0);
+		double maxLabor = jobs.get(0).getLaborCost();
+		for (int i = 1; i < jobs.size(); i++) {
+			if (jobs.get(i).getLaborCost() > maxLabor) {
+				mostLabor = jobs.get(i);
+				maxLabor = jobs.get(i).getLaborCost();
+			}
+		}
+		return mostLabor;
+	}
+	
+	public static Job getMinlaborJob(ArrayList<Job> jobs) {
+		Job leastLabor = jobs.get(0);
+		double minLabor = jobs.get(0).getLaborCost();
+		for (int i = 1; i < jobs.size(); i++) {
+			if (jobs.get(i).getLaborCost() > minLabor) {
+				leastLabor = jobs.get(i);
+				minLabor = jobs.get(i).getLaborCost();
+			}
+		}
+		return leastLabor;
+	}
 
 }
